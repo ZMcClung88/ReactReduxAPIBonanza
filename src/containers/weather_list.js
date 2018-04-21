46901f9839458823791703bd680b91dc8ce1cee5
Zac McClung <zac.mcclung370@gmail.com>
2018-04-21 10:26:13 -0600

    saving changes

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import Chart from '../components/chart';
-// import GoogleMap from '../components/google_map';
+// import _ from 'lodash';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     console.log('City Data', cityData);
+    const name = cityData.city.name;
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), temp => _.round(9 / 5 * (temp - 273) + 32));
+    console.log(temps);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
+
+    return (
+      <tr key={name}>
+        <td>
+          <GoogleMap lon={lon} lat={lat} />
+        </td>
+        <td>
+          <Chart data={temps} color="deeppink" units="F" />
+        </td>
+        <td>
+          <Chart data={pressures} color="chartreuse" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidities} color="aqua" units="%" />
+        </td>
+      </tr>
+    );
   }
   render() {
     return (
